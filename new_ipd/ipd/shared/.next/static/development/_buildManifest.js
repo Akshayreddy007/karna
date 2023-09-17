3ed383f46e0e9b180edac9335c1bbcd7e53cb29e
Akshayreddy007 <95066836+Akshayreddy007@users.noreply.github.com>
2023-09-17 17:22:56 +0530

    new

---
+++ new_ipd/ipd/shared/.next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/ipr_appointments":["static\u002Fchunks\u002Fpages\u002Fipr_appointments.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fipr_appointments"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
